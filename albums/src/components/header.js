8c67343f9a8ef170b06915c14a3a4fc386b1bb3a
parag03 <parag.somani0308@gmail.com>
2017-03-15 00:11:11 +0530

    made sample app

---
+++ albums/src/components/header.js
@@ -0,0 +1,15 @@
+
+
+//import libarary to make component
+import React from 'react';
+import { Text } from 'react-native';
+
+//make a component
+const Header = () => {
+ return (
+ 	<Text>Albums!</Text>
+ 	);
+};
+
+//make a component available to other part of app
+export default Header;
